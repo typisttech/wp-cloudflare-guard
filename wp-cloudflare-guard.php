d693d70d9d1d3d7e08a7e2b3354a035fea233576
Tang Rufus <tangrufus@gmail.com>
2017-04-19 20:39:59 +0800

    Remove Activator and Deactivator

--- wp-cloudflare-guard.php
+++ wp-cloudflare-guard.php
@@ -40,16 +40,6 @@ if (! defined('WPINC')) {
 
 require_once plugin_dir_path(__FILE__) . 'vendor/autoload.php';
 
-/*
- * The code that runs during plugin activation.
- */
-register_activation_hook(__FILE__, [ Activator::class, 'activate' ]);
-
-/*
- * The code that runs during plugin deactivation.
- */
-register_deactivation_hook(__FILE__, [ Deactivator::class, 'deactivate' ]);
-
 /**
  * Begins execution of the plugin.
  * Since everything within the plugin is registered via hooks,
